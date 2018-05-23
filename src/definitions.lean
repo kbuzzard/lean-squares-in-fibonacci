9a9657273764594219b7554f2379db9b5a2dca45
Nicholas Scheel <monoidmusician@gmail.com>
2018-05-23 15:15:51 -0500

    Add Z[α], Fib : Z -> Z
    αβsum and αβprod are now trivial

--- src/definitions.lean
+++ src/definitions.lean
@@ -6,23 +6,14 @@ noncomputable theory
 def α := (real.sqrt 5 + 1) / 2
 def β := 1 - α 
 
-lemma αβsum : α + β = 1 := begin
-  unfold β,
-  unfold α,
-  norm_num, -- ;-)
-end 
-
-lemma αβprod : α * β = -1 := begin
-  unfold β,
-  unfold α,
-  rw [mul_sub,mul_one,add_div,mul_add,add_mul,add_mul], -- meh 
-  sorry -- :-)
-end
-
 def fib : ℕ → ℕ
 | 0 := 0
 | 1 := 1
 | (n + 2) := fib n + fib (n + 1)
+
+def Fib : ℤ → ℤ
+| (int.of_nat n) := fib n
+| -[1+n] := (-1)^n * fib (n+1)
 
 def luc : ℕ → ℕ
 | 0 := 2
