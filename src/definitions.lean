de2ecaeb8075c7a4eeefcbaa5d38d2be0b3454b6
kckennylau <kc_kennylau@yahoo.com.hk>
2018-05-23 21:57:20 +0100

    point 4

--- src/definitions.lean
+++ src/definitions.lean
@@ -19,3 +19,14 @@ def luc : ℕ → ℕ
 | 0 := 2
 | 1 := 1
 | (n + 2) := luc n + luc (n + 1)
+
+local attribute [elab_as_eliminator] nat.strong_induction_on
+
+@[elab_as_eliminator]
+protected def nat.rec_on_two {C : ℕ → Sort*} (n : ℕ)
+  (H0 : C 0) (H1 : C 1)
+  (ih : Π (n : ℕ), C n → C (nat.succ n) → C (nat.succ (nat.succ n))) :
+  C n :=
+nat.strong_induction_on n $ λ n, nat.cases_on n (λ _, H0) $
+λ n, nat.cases_on n (λ _, H1) $ λ n ih2, ih n (ih2 n $ nat.lt_succ_of_lt $ nat.le_refl _) $
+ih2 (n+1) $ nat.le_refl _
--- src/point4.lean
+++ src/point4.lean
@@ -1,44 +1,20 @@
 import definitions 
 
-lemma point_4_aux (m : ℕ) : 
-(luc (m + 3) = 2 * fib (m + 3) + fib m) ∧ (luc (m + 4) = 2 * fib (m + 4) + fib (m + 1)) :=
+local attribute [elab_as_eliminator] nat.strong_induction_on
 
-begin
-  induction m with d Hd,
-  { -- base case
-    split;refl
-  },
-  { split,
-      exact Hd.right,
-    show (luc (d + 3) + luc (d + 4)) = 2 * (fib (d + 3) + fib (d + 4)) + (fib d + fib (d + 1)),
-    rw [Hd.1,Hd.2],
-    simp [mul_add],
-  }
-end
-
-lemma point_4_part_a (m : ℕ) : luc (m + 3) = 2 * fib (m + 3) + fib m := (aux m).left 
+lemma luc_fib (m : ℕ) : luc (m + 3) = 2 * fib (m + 3) + fib m :=
+nat.rec_on_two m rfl rfl $ λ n ih1 ih2,
+show (luc (n + 3) + luc (nat.succ n + 3)) = 2 * (fib (n + 3) + fib (n + 4)) + (fib n + fib (n + 1)),
+by rw [ih1, ih2, mul_add]; simp
 
 open nat
 
-#check gcd_rec 
+lemma nat.gcd_add (a b : ℕ) : gcd (a + b) b = gcd a b :=
+by rw [gcd_comm, gcd_rec, add_mod_right, ← gcd_rec, gcd_comm]
 
-lemma nat.gcd_aux (a b : ℕ) : gcd (a + b) b = gcd a b := 
-calc gcd (a + b) b = gcd b (a + b) : gcd_comm _ _
-...                = gcd ((a + b) % b) b : gcd_rec _ _
-...                = gcd (a % b) b : by simp
-...                = gcd b a : (gcd_rec _ _).symm 
-...                = gcd a b : gcd_comm _ _
- 
-lemma point_4_part_b (m : ℕ) : nat.gcd (fib (m + 1)) (fib m) = 1 := 
-begin
-  induction m with d Hd,
-  { -- base case 
-    exact dec_trivial
-  },
-  { -- inductive step
-    show nat.gcd (fib d + fib (d + 1)) (fib (d + 1)) = 1,    
-    rwa [nat.gcd_aux,gcd_comm]
-  }
-end 
+lemma gcd_fib_succ (m : ℕ) : nat.gcd (fib (m + 1)) (fib m) = 1 :=
+nat.rec_on m dec_trivial $ λ n ih,
+show gcd (fib n + fib (n+1)) (fib (n+1)) = 1,
+by rw [nat.gcd_add, gcd_comm, ih]
 
 -- TODO -- gcd fib and luc is 1 or 2
